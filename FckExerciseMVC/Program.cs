4adb81ac5954f75465275b884a684de745498eb7
Tropson <tropson90@gmail.com>
2019-03-07 12:04:09 +0100

    First

--- FckExerciseMVC/Program.cs
+++ FckExerciseMVC/Program.cs
@@ -14,6 +14,7 @@ namespace FckExerciseMVC
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
             CreateWebHostBuilder(args).Build().Run();
         }
 
